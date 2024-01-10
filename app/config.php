b1584de7fb34b677e0d5c2c6e8365ab9ceec3f92
Toshihide <93624688+hide338@users.noreply.github.com>
2024-01-10 12:07:41 +0900

    Update config.php

--- app/config.php
+++ app/config.php
@@ -3,8 +3,8 @@
 // session_start();
 date_default_timezone_set('Asia/Tokyo');
 define('TODAY', date('Ymd'));
-define('DSN', 'sqlsrv:server=SUKAGAWASRV14;database=sukagawadb');
-define('DB_USER', 'viewer');
+define('DSN', '');
+define('DB_USER', '');
 define('DB_PASS', '');
 
 require_once( dirname(__FILE__) . '/Utils.php');
